d0056595c5444c2fc171a746251d47d3e18e7261
Jacob Feuerbach <jacob.feuerbach@gmail.com>
2018-09-28 23:07:15 -0500

    Installed Bootstrap and NgBootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { UiowaAccountModule } from 'projects/uiowa-account/src/public_api';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, UiowaAccountModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
